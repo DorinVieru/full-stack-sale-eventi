f9322aca777659692c8dff9ac3354e437bd06887
DorinVieru <dorino.vieru@gmail.com>
2024-06-29 15:30:54 +0200

    UP: Improve code of dashboard and welcome page and change language of profile

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,7 +8,7 @@
     <div class="row justify-content-center">
         <div class="col">
             <div class="card">
-                <div class="card-header">{{ __('User Dashboard') }}</div>
+                <div class="card-header">{{ __('Dashboard Utente') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -18,6 +18,18 @@
                     @endif
 
                     {{ __('Benvenuto nel tuo gestionale!') }}
+                    <div class="my-5">
+                            <a href="{{ route('admin.rooms.index') }}"
+                                class="link-dash text-decoration-none bg-body-secondary p-3 rounded-4 text-dark {{ Route::currentRouteName() == 'admin.rooms.index' ? 'bg-secondary' : '' }}">
+                                Elecono delle sale Meeting
+                            </a>
+                    </div>
+                    <div class="mb-4">
+                        <a href="{{ route('admin.events.index') }}"
+                            class="link-dash text-decoration-none bg-body-secondary p-3 rounded-4 text-dark {{ Route::currentRouteName() == 'admin.events.index' ? 'bg-secondary' : '' }}">
+                            Eventi in programma
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
